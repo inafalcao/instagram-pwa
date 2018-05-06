e341cfb92152692d5ffd31408245c262a1b11664
Inaciane Falcão <inafalcao@gmail.com>
2018-05-06 20:14:34 -0300

    Postponing homescreen installation prompt.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,8 +1,18 @@
 
+var deferredPrompt
+
+
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('/sw.js' /* {scope: '/'} */) // register scope here if necessary.
 		.then(function() {
-			console.log('Service worker registered!');
-		});
+			console.log('Service worker registered!')
+		})
 }
+
+window.addEventListener('beforeinstallprompt', function(event) {
+	console.log('beforeinstallprompt fired')
+	event.preventDefault()
+	deferredPrompt = event
+	return false
+})
--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,21 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  if(deferredPrompt) {
+  	deferredPrompt.prompt()
+
+  	deferredPrompt.userChoice.then(function(choiseResult) {
+  		console.log(choiseResult.outcome)
+
+  		if(choiseResult.outcome === 'dismiss') {
+  			console.log('User cancelled installation')
+  		} else {
+  			console.log('User added to home screen')
+  		}
+  		
+  		deferredPrompt = null
+  	})
+  }
 }
 
 function closeCreatePostModal() {
