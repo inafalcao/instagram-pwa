1e5eeb748d558159c34776db9de2b4d0022af972
Inaciane Falcão <inafalcao@gmail.com>
2018-05-09 22:45:12 -0300

    Cleaning up some code, adding new card with image.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,12 +1,18 @@
 
 var deferredPrompt
 
+if(!window.Promise) {
+	windows.Promise = Promise
+}
 
 if('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('/sw.js' /* {scope: '/'} */) // register scope here if necessary.
 		.then(function() {
 			console.log('Service worker registered!')
+		})
+		.catch(function(err) {
+			console.log('Error registering service worker', err)
 		})
 }
 
--- public/sw.js
+++ public/sw.js
@@ -3,13 +3,9 @@ self.addEventListener('install', function(event) {
 })
 
 self.addEventListener('activate', function(event) {
-	console.log('[Service Worker] Activating service worker.', event)
 	return self.clients.claim() // todo: research more about this
 })
 
 self.addEventListener('fetch', function(event) {
-	console.log('[Service Worker] Fetching inside worker.', event)
-
 	event.respondWith(fetch(event.request))
-
 })
